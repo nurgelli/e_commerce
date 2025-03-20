3b9a8ffdfc2eb51705991bddccd75636e6372354
nurgelli <nt.interpretor@gmail.com>
2025-03-20 19:27:26 +0500

    Added serilizers and views

--- ecom/views.py
+++ ecom/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Category, SubCategory, Product
+from .serializers import CategorySerializer, ProductSerializer, SubCategorySerializer
 
-# Create your views here.
+
+class CategoryListAPI(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class ProductListAPI(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+class SubCategoryListAPI(ListAPIView):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+    
+    
+class ProductDetailAPI(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
