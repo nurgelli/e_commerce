ec826513e2ed2084c0a361cf97ff8f4e1cd22288
nurgelli <nt.interpretor@gmail.com>
2025-03-21 17:19:30 +0500

    Swagger and API added

--- configuration/settings.py
+++ configuration/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ecom',
     'rest_framework',
-    'drf-yasg'
+    'drf_yasg'
     
 ]
 
--- configuration/urls.py
+++ configuration/urls.py
@@ -15,11 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+        description="API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecom/', include('ecom.urls'))
+    path('ecom/', include('ecom.urls')),
     
+    # Swagger and Redoc URLs
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger\.json$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
