18b16bc6c289c81e1a1c79b62708e9675b3d2ee7
nurgelli <nt.interpretor@gmail.com>
2025-03-21 15:17:19 +0500

    Swagger added

--- ecom/models.py
+++ ecom/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    icon = models.ImageField(upload_to="products/", blank=True, null=True)
     slug = models.SlugField(unique=True)
     
     def __str__(self):
