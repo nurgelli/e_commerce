7c1dbe092f39992037f231450b76324525b87e41
nurgelli <nt.interpretor@gmail.com>
2025-06-16 13:55:09 +0500

    this is ecom initilized

--- product/admin.py
+++ product/admin.py
@@ -1,29 +1,3 @@
 from django.contrib import admin
-from unfold.decorators import action
-from unfold.decorators import display
-from django.contrib.admin import TabularInline
-from product.models import Product, Storage
-from unfold.admin import ModelAdmin
 
-
-# admin.site.register(Product)
-# admin.site.register(Storage)
-
-@admin.register(Product)
-class ProductAdmin(ModelAdmin):
-    list_display = ('name', 'price', 'category')
-    search_fields = ['name']
-    icon = "box"  # Unfold icon name from lucide.dev
-    fieldsets = (
-        ("Basic Info", {"fields": ("name", "slug", "description")}),
-        ("Pricing", {"fields": ("price",)}),
-        ("Categorization", {"fields": ("category", "subcategory")}),
-        ("Image", {"fields": ("image",)}),
-        # ("Timestamps", {"fields": ("created_at", "updated_at")}),
-    )
-    
-@admin.register(Storage)
-class StorageAdmin(ModelAdmin):
-    list_display = ('product', 'type_of_storage', 'total_quantity') 
-    icon = "database"
-
+# Register your models here.
--- product/views.py
+++ product/views.py
@@ -1,21 +1,3 @@
 from django.shortcuts import render
-from ecom.serializers import ProductSerializer, StorageSerializer
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-from .models import Product, Storage
-
-
-class ProductListAPI(ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
-    
-class ProductDetailAPI(RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'slug'
-
-class StorageListAPI(ListAPIView):
-    queryset = Storage.objects.all()
-    serializer_class = StorageSerializer
-    
+# Create your views here.
