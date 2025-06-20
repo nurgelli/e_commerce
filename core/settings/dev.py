bdbf6ff2dec5db9286127011081a784c5cd69697
nurgelli <nt.interpretor@gmail.com>
2025-06-20 13:10:23 +0500

    settings name and things

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
